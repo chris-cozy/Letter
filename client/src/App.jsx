3b1a5000ba225f49b393d72c0516c20439ceaa66
chris-cozy <csande9@clemson.edu>
2023-10-16 12:23:46 -0400

    Included register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
+import Register from "./components/auth/Register";
+
 
 function App() {
 
   return (
     <>
-      <div className='bg-red-500'>
-        test
-      </div>
+      <Register />
     </>
   )
 }
