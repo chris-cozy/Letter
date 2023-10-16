25a431b01c8bce8cfa254f62d099dfaf41cc069a
chris-cozy <csande9@clemson.edu>
2023-10-16 12:22:18 -0400

    Reconfigured tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/*.jsx"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {},
   },
