69aad8917418a1fb25f4861e42f77d432b0d7929
chris-cozy <csande9@clemson.edu>
2023-10-16 13:12:19 -0400

    Included axios and connected to server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import Register from "./components/auth/Register";
+import axios from "axios"
 
 
 function App() {
-
+  axios.defaults.baseURL = 'http://localhost:4040';
+  axios.defaults.withCredentials = true;
   return (
     <>
       <Register />
