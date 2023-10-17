faec9b3bd844f7b4a260cf7275079060f53dcec4
chris-cozy <csande9@clemson.edu>
2023-10-16 23:33:27 -0400

    Updated the color scheme

--- client/src/components/auth/Authentication.jsx
+++ client/src/components/auth/Authentication.jsx
@@ -19,17 +19,22 @@ export default function Authentication() {
     }
 
     return (
-        <div className="bg-blue-50 h-screen flex items-center">
-            <form className="w-64 mx-auto mb-12" onSubmit={handleSubmit}>
-                <input value={username} onChange={ev => setUsername(ev.target.value)} type="text" placeholder="username" className="block w-full rounded-lg p-2 mb-2 border" />
-                <input value={password}  onChange={ev => setPassword(ev.target.value)} type="password" placeholder="password" className="block w-full rounded-lg p-2 mb-2 border" />
-                <button className="bg-blue-500 text-white block w-full rounded-lg p-2">{isLogin ? 'Login' : 'Register'}</button>
-                {isLogin ? <div className="text-center mt-2">Don't have an account?
-                    <button onClick={() => setIsLogin(!isLogin)}>Register Here</button>
-                </div> : <div className="text-center mt-2">Already a member? 
-                    <button onClick={() => setIsLogin(!isLogin)}>Login Here</button>
-                </div>}
-            </form>
-        </div>
+        <>
+            <div className="bg-background h-screen flex flex-col items-center justify-center">
+                <h1 className="text-4xl font-bold mb-8 text-primary">Letter</h1>
+                <p className="text-lg mb-8 text-text">messages that matter, moments that connect</p>
+                <form className="w-64 mx-auto mb-12" onSubmit={handleSubmit}>
+                    <input value={username} onChange={ev => setUsername(ev.target.value)} type="text" placeholder="username" className="block w-full rounded-lg p-2 mb-2 border" />
+                    <input value={password}  onChange={ev => setPassword(ev.target.value)} type="password" placeholder="password" className="block w-full rounded-lg p-2 mb-2 border" />
+                    <button className="bg-blue-500 hover:bg-secondary text-white block w-full rounded-lg p-2">{isLogin ? 'Login' : 'Register'}</button>
+                    {isLogin ? <div className="text-center mt-2">Don't have an account?
+                        <button onClick={() => setIsLogin(!isLogin)}>Register Here</button>
+                    </div> : <div className="text-center mt-2">Already a member? 
+                        <button onClick={() => setIsLogin(!isLogin)}>Login Here</button>
+                    </div>}
+                </form>
+            </div>
+        </>
+        
     )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3498db",
+        secondary: "#2ecc71",
+        accent: "#e74c3c",
+        background: "#f2f3f4",
+        text: "#2c3e50",
+      },
+    },
   },
+  variants: {},
   plugins: [],
 };
